271369ed2ad902b70ae582774114340f8e7bd25e
Alfred (Mac) <59073967+slimer37@users.noreply.github.com>
2025-03-07 22:06:21 -0800

    tab layout sample

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,6 +1,6 @@
 import { Text, View } from "react-native";
 
-export default function Index() {
+export default function Index2() {
   return (
     <View
       style={{
@@ -10,6 +10,7 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Hello there!</Text>
     </View>
   );
 }
