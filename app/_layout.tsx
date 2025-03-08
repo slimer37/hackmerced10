94a1abedb6949da090cfca973fdec73fb0b0e96e
Alfred (Mac) <59073967+slimer37@users.noreply.github.com>
2025-03-08 12:16:59 -0800

    add auth0 login sample

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,60 @@
 import { Stack } from 'expo-router/stack';
+import { useAuth0, Auth0Provider } from 'react-native-auth0';
+import React from 'react';
+import {Button, Text, View, StyleSheet} from 'react-native';
+
+function App() {
+  const {authorize, clearSession, user, error, isLoading} = useAuth0();
+
+  const onLogin = async () => {
+    try {
+      await authorize();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const onLogout = async () => {
+    try {
+      await clearSession();
+    } catch (e) {
+      console.log('Log out cancelled');
+    }
+  };
+
+  if (isLoading) {
+    return <View style={styles.container}><Text>Loading</Text></View>;
+  }
+
+  const loggedIn = user !== undefined && user !== null;
+
+  return (
+    <View style={styles.container}>
+      {loggedIn && <Text>You are logged in as {user.name}</Text>}
+      {!loggedIn && <Text>You are not logged in</Text>}
+      {error && <Text>{error.message}</Text>}
+
+      <Button
+        onPress={loggedIn ? onLogout : onLogin}
+        title={loggedIn ? 'Log Out' : 'Log In'}
+      />
+    </View>
+  );
+}
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="profile" options={{ headerShown: true }} />
-    </Stack>
-  );
+    <Auth0Provider domain={"dev-g764byi3mgr8fsp2.us.auth0.com"} clientId={"rPPrr9pgCWMg91o2JykRJkPHpcje15RU"}>
+      <App/>
+    </Auth0Provider>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  }
+});
