f99036dd1c6e6de829634cb097201641f596bd4b
Alfred (Mac) <59073967+slimer37@users.noreply.github.com>
2025-03-08 23:35:39 -0800

    3 tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -36,13 +36,6 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="medicinesearch"
-        options={{
-          title: 'Find & Restock',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="search" color={color} />,
-        }}
-      />
-      <Tabs.Screen
         name="mymedicine"
         options={{
           title: 'My Medicine',
